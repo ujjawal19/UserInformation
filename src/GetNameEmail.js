2e0f0cbd7dab0ac6c20b794fc1a65779d5d7e4a9
Ujjawal Mathur <ujjawalmathur.mathur@gmail.com>
2022-12-01 21:24:30 +0530

    UI Fixes and created Readme File

--- src/GetNameEmail.js
+++ src/GetNameEmail.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import axios from 'axios';
+import './App.css';
 const GetNameEmail = () => {
   const [user, setUser] = useState([]);
   const [localdata, setLocaldata] = useState([]);
@@ -18,8 +19,8 @@ const GetNameEmail = () => {
     if (user.length) {
       console.log("hello", user)
       const _data = {
-        "Gender": user[0]?.gender,
-        "Name": `${user[0]?.name.first} ${user[0]?.name.last}`
+        "Address": `${user[0]?.location.street.number} ${user[0]?.location.street.name} ${user[0]?.location.city}`,
+        "Name": `${user[0]?.name.title} ${user[0]?.name.first} ${user[0]?.name.last}`
       }
       
       setLocaldata((prev) => [_data, ...prev]);
@@ -28,18 +29,18 @@ const GetNameEmail = () => {
 
   return (
     <div className='parent' >
-      <span className='title'>User Information</span>
+      <div className='heading'>User Information</div>
       {user?.map((data,idx) => (
         <div key={idx} className='cardWrapper'>
-          <p><span className='key'>Gender:</span> {data.gender}</p>
-          <p><span className='key'>Name:</span> {data.name.first}</p>
+          <p><span className='key'>Address:</span> {data.location.street.number} {data.location.street.name}{data.location.city}</p>
+          <p><span className='key'>Name:</span> {data.name.title} {data.name.first} {data.name.last}</p>
         </div>
       ))}
       <button onClick={fetchData}>Refersh</button>
       <div className='localDataWrapper' >
         {localdata?.map((data,idx) => (
           <div key={idx}  className='cardWrapper'>
-            <p><span className='key'>Gender:</span> {data.Gender}</p>
+            <p><span className='key'>Address:</span> {data.Address}</p>
             <p><span className='key'>Name:</span> {data.Name}</p>
           </div>
         ))}
@@ -48,4 +49,4 @@ const GetNameEmail = () => {
   )
 }
 
-export default GetNameEmail;
+export default GetNameEmail
