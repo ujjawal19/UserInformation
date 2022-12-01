586c7ffef0aaf8d896b19107937aadbe2c8dc555
Ujjawal Mathur <ujjawalmathur.mathur@gmail.com>
2022-12-01 19:47:12 +0530

    Integrated User Info UI and API

--- src/GetNameEmail.js
+++ src/GetNameEmail.js
@@ -2,26 +2,48 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 const GetNameEmail = () => {
   const [user, setUser] = useState([]);
+  const [localdata, setLocaldata] = useState([]);
 
   const fetchData = async () => {
     const { data } = await axios.get("https://randomuser.me/api/");
-    console.log("RESPONSE:", data);
     const user = data.results;
     setUser(user);
   }
+
   useEffect(() => {
     fetchData();
-
   }, [])
-  console.log("Data :-",user);
+  console.log(user);
+  useEffect(() => {
+    if (user.length) {
+      console.log("hello", user)
+      const _data = {
+        "Gender": user[0]?.gender,
+        "Name": `${user[0]?.name.first} ${user[0]?.name.last}`
+      }
+      
+      setLocaldata((prev) => [_data, ...prev]);
+    }
+  }, [user])
+
   return (
-    <div>
-      {user.map(user => (
-          <div key={user.login.uuid}>
-            <h1>Gender: {user.gender}</h1>
-            <h1>Name : {user.name.first} {user.name.last}</h1>
-          </div>  
+    <div className='parent' >
+      <span className='title'>User Information</span>
+      {user?.map((data,idx) => (
+        <div key={idx} className='cardWrapper'>
+          <p><span className='key'>Gender:</span> {data.gender}</p>
+          <p><span className='key'>Name:</span> {data.name.first}</p>
+        </div>
+      ))}
+      <button onClick={fetchData}>Refersh</button>
+      <div className='localDataWrapper' >
+        {localdata?.map((data,idx) => (
+          <div key={idx}  className='cardWrapper'>
+            <p><span className='key'>Gender:</span> {data.Gender}</p>
+            <p><span className='key'>Name:</span> {data.Name}</p>
+          </div>
         ))}
+      </div>
     </div>
   )
 }
